6f840622e0460a91f4d004c7e22ba44cd16540d0
Ramon Alvarez Rivera <ramonalvarezrivera25@gmail.com>
2023-11-12 12:01:23 -0600

    Add basic examples whit Options API and Composition API

---
+++ src/examples/ContadorComposition.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue'
+    
+    const count = ref(0);
+
+    function aumentar(){
+        count.value ++;
+    }
+
+    function disminuir(){
+        count.value --;
+    }
+
+    onMounted(function (){
+        console.log("El valor inicial del contador es: " + count.value);
+    });
+
+</script>
+
+<template>
+    <p>Soy la version con Composition API</p>
+    <h1>Contador: {{ count }}</h1>
+
+    <button @click = aumentar>
+        Incrementar Contador en 1
+    </button>
+
+    <button @click = disminuir>
+        Disminuir Contador en 1
+    </button>
+
+</template>
